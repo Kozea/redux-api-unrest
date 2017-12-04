28b387e7a37d6131bb42fd1ca12f83095c345be5
Florian Mounier <florian.mounier@kozea.fr>
2017-12-04 18:00:09 +0100

    Add a defaulter func

--- src/options.js
+++ src/options.js
@@ -4,3 +4,17 @@ export const defaults = {
   transformer,
   urlOptions: { qsStringifyOptions: { indices: false } },
 }
+
+export const withDefaults = endpoints =>
+  Object.entries(endpoints).reduce((ep, [key, val]) => {
+    if (typeof val === 'string') {
+      val = {
+        url: val,
+      }
+    }
+    ep[key] = {
+      ...defaults,
+      ...val,
+    }
+    return ep
+  }, {})
