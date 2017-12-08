a1dac7dd429dace744ffadc29148e4b1bfcafe36
Florian Mounier <florian.mounier@kozea.fr>
2017-12-08 10:43:53 +0100

    Test JWTStorage

---
+++ test/misc.test.js
@@ -0,0 +1,127 @@
+import { applyMiddleware, combineReducers, createStore } from 'redux'
+import thunk from 'redux-thunk'
+import regeneratorRuntime from 'regenerator-runtime'
+
+import ApiUnrest from '../src'
+// eslint-disable-next-line no-unused-vars
+import { timeout } from './utils'
+
+describe('Handles jwt', () => {
+  it('sends the token on fetch request', async () => {
+    const storage = {
+      getItem: name => storage[name],
+      setItem: (name, value) => (storage[name] = value),
+      removeItem: name => delete storage[name],
+      jwt: 'JWTTOKEN',
+    }
+
+    const api = new ApiUnrest(
+      {
+        fruit: 'fruit',
+        color: 'base/color/:id?',
+        tree: 'forest/tree/:type?/:age?',
+      },
+      {
+        JWTStorage: storage,
+        fetch: async (url, opts) => {
+          await timeout(25)
+          return {
+            status: 200,
+            headers: {
+              get: key =>
+                ({
+                  'Content-Type': 'application/json',
+                }[key]),
+            },
+            // eslint-disable-next-line require-await
+            json: async () => ({
+              objects: [{ headers: opts.headers }],
+            }),
+          }
+        },
+      }
+    )
+    const store = createStore(
+      combineReducers(api.reducers),
+      applyMiddleware(thunk)
+    )
+    await store.dispatch(api.actions.color.get())
+    expect(store.getState().color.objects[0].headers.Authorization).toEqual(
+      'Bearer JWTTOKEN'
+    )
+  })
+  it('sets the token on fetch response', async () => {
+    const storage = {
+      getItem: name => storage[name],
+      setItem: (name, value) => (storage[name] = value),
+      removeItem: name => delete storage[name],
+      jwt: 'OLDTOKEN',
+    }
+    const api = new ApiUnrest(
+      {
+        fruit: 'fruit',
+        color: 'base/color/:id?',
+        tree: 'forest/tree/:type?/:age?',
+      },
+      {
+        JWTStorage: storage,
+        fetch: async () => {
+          await timeout(25)
+          return {
+            status: 200,
+            headers: {
+              get: key =>
+                ({
+                  'Content-Type': 'application/json',
+                  Authorization: 'JWTTOKEN',
+                }[key]),
+            },
+            // eslint-disable-next-line require-await
+            json: async () => ({}),
+          }
+        },
+      }
+    )
+    await api.actions.color.get()(() => ({}))
+    expect(storage.jwt).toEqual('JWTTOKEN')
+  })
+  it('removes the token on a 401', async () => {
+    const storage = {
+      getItem: name => storage[name],
+      setItem: (name, value) => (storage[name] = value),
+      removeItem: name => delete storage[name],
+      jwt: 'shouldBeRemoved',
+    }
+    const api = new ApiUnrest(
+      {
+        fruit: 'fruit',
+        color: 'base/color/:id?',
+        tree: 'forest/tree/:type?/:age?',
+      },
+      {
+        JWTStorage: storage,
+        fetch: async () => {
+          await timeout(25)
+          return {
+            status: 401,
+            headers: {
+              get: key =>
+                ({
+                  'Content-Type': 'application/json',
+                  Authorization: 'BADJWTTOKEN',
+                }[key]),
+            },
+            // eslint-disable-next-line require-await
+            json: async () => ({}),
+          }
+        },
+      }
+    )
+    try {
+      await api.actions.color.get()(() => ({}))
+    } catch (err) {
+      expect()
+    }
+    expect(storage.jwt).toBeUndefined()
+  })
+})
